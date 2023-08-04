2f12cd3e1744e12e8007d6d27d37dd3385f1d7cd
Reevewho <141252419+Reevewho@users.noreply.github.com>
2023-08-04 15:07:48 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -35,35 +35,35 @@ if (isOnHomePage()) {
     const navbar = document.getElementById("navbar");
     navbar.classList.add("navbar-solid");
 }
+    let slideIndex = 1;
+    showSlides(slideIndex);
 
-let slideIndex = 1;
-showSlides(slideIndex);
+    function plusSlides(n) {
+        showSlides(slideIndex += n);
+    }
 
-function plusSlides(n) {
-    showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
+    function currentSlide(n) {
+        showSlides(slideIndex = n);
+    }
 
 function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("demo");
-  let captionText = document.getElementById("caption");
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("demo");
+    let captionText = document.getElementById("caption");
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
     }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
+    // Remove the "active" class from all dots
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
     }
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
-  captionText.innerHTML = dots[slideIndex-1].alt;
-    }
+  captionText.innerHTML = dots[slideIndex - 1].alt;
+  }
     
 //form input storage
 function response() {
