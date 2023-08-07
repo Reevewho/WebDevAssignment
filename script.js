2e50487d1d89899462ca5a24552ba2790c8d1b4d
Reevewho <141252419+Reevewho@users.noreply.github.com>
2023-08-07 09:51:01 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -66,83 +66,130 @@ function showresponse(){
     document.getElementById("msg-response").innerText = newMsg;
 }
 
+
 let imageUrls = [
-    "rezeroln.png",
-    "NGNLln.png",
-    "konosubaln.jpg",
-    "Coteln.png",
-    "oregairuln.png",
-    "chitoseln.png"
-    // Add more image filenames as needed
-  ];
+  {
+    filename: "rezeroln.png",
+    title: "Re:Zero Starting life in another world from zero",
+    description: "One of my most favourite isekai novels of all time where the protagonist is gifted the power to return back to a save point everytime he dies.",
+  },
+  {
+    filename: "NGNLln.png",
+    title: "No Game No Life",
+    description: "Another one of my favourite isekais where a gamer duo is dropped into a world where games are everything. Seeing their journey to world domination is interesting.",
+  },
+  {
+    filename: "konosubaln.jpg",
+    title: "Gods Blessings on this wonderful world",
+    description: "One of the funniest isekais novel ever, if not the funniest. The cast is very loveable and i feel like im part of the game whenever i read about their journeys.",
+  },
+  {
+    filename: "Coteln.png",
+    title: "Classroom of the elite",
+    description: "A very interesting novel where the protagonist is hiding many secrets from not just the people around him but from us, the readers as well.",
+  },
+  {
+    filename: "oregairuln.png",
+    title: "My Teenage Romantic Comedy Snafu",
+    description: "One of my favourite slice of life novels due to its very relatable protagonist initially. I have since developed and matured with him as a person.",
+  },
+  {
+    filename: "chitoseln.png",
+    title: "Chitose Is In The Ramune Bottle",
+    description: "A very interesting take on the slice of life genre with the concept of the novel revolving around 'Riajuus' aka Normies POV of the world",
+  },
+  // Add more image filenames as needed
+];
 
 function addImagesToContainer() {
-    const container = document.querySelector(".image-container");
-    container.innerHTML = ""; // Clear existing content before adding images
+  const container = document.querySelector(".image-container");
+  container.innerHTML = ""; // Clear existing content before adding images
 
-    // Increase the number of duplications to make the loop happen faster
-    const numberOfDuplications = 20; // You can adjust this value as needed
-    const duplicateImageUrls = [];
-    for (let i = 0; i < numberOfDuplications; i++) {
-      duplicateImageUrls.push(...imageUrls);
+  const numberOfDuplications = 20; // You can adjust this value as needed
+  const duplicateImageUrls = [];
+  for (let i = 0; i < numberOfDuplications; i++) {
+    duplicateImageUrls.push(...imageUrls);
+  }
+
+  duplicateImageUrls.forEach((imageDetails) => {
+    const imageUrl = `images/${imageDetails.filename}`;
+    const div = document.createElement("div");
+    div.style.backgroundImage = `url(${imageUrl})`;
+
+    // Create an image element and add it to the div
+    const imageElement = new Image();
+    imageElement.src = imageUrl;
+
+    // Set the image size to match the desired size in the CSS
+    imageElement.style.maxWidth = "60vw";
+    imageElement.style.maxHeight = "60vh";
+
+    div.appendChild(imageElement);
+
+    // Update the event listener to open the modal with the specific image details
+      imageElement.addEventListener("click", () => openModal(imageDetails));
+
+    container.appendChild(div);
+  });
+}
+
+let currentX = 0;
+const distanceToMove = 0.5; // Adjust this value to control the animation speed
+
+function moveImages() {
+  const container = document.querySelector(".image-container");
+  const containerWidth = container.clientWidth;
+
+  // Calculate the total width of images and the duration for the animation
+  const imageCount = imageUrls.length;
+  const totalImageWidth = imageCount * (containerWidth / 4); // Display 4 images at a time
+  const duration = totalImageWidth / distanceToMove; // Adjust the duration based on the distance to move
+
+  // Calculate the distance to move based on time
+  currentX -= distanceToMove;
+  if (currentX <= -(totalImageWidth - containerWidth)) {
+    currentX = 0;
+  }
+
+  // Move the container
+  container.style.transform = `translateX(${currentX}px)`;
+
+  // Request the next frame
+  requestAnimationFrame(moveImages);
+
+  // Adjust the duration to maintain a smooth animation
+  container.style.animation = `scrollAnimation ${duration}s linear infinite`;
+}
+
+// Start the animation on page load
+moveImages();
+
+function openModal(imageDetails) {
+  const modal = document.getElementById("modal");
+  const modalImage = document.getElementById("modalImage");
+  const modalTitle = document.getElementById("modalTitle");
+  const modalDescription = document.getElementById("modalDescription");
+  const closeBtn = document.getElementById("close");
+
+  const imageUrl = `images/${imageDetails.filename}`;
+  modalImage.innerHTML = ""; // Clear previous image, if any
+  const imageElement = new Image();
+  imageElement.src = imageUrl;
+  imageElement.style.maxWidth = "100%"; // To fit the modal content
+  imageElement.style.maxHeight = "100%";
+  modalImage.appendChild(imageElement);
+
+  modalTitle.textContent = imageDetails.title;
+  modalDescription.textContent = imageDetails.description;
+  modal.style.display = "block";
+
+  closeBtn.onclick = () => closeModal();
+  modal.onclick = (event) => {
+    if (event.target === modal || event.target === closeBtn) {
+      closeModal();
     }
-
-    duplicateImageUrls.forEach((url) => {
-      const imageUrl = `images/${url}`; // Assuming images are in a folder named 'images'
-      const div = document.createElement("div");
-      div.style.backgroundImage = `url(${imageUrl})`;
-      div.addEventListener("click", () => openModal(imageUrl)); // Add click event listener
-      container.appendChild(div);
-    });
-
-    // Calculate the total width of images and set it as the container width
-    const images = container.children;
-    const totalImageWidth = images.length * (images[0].clientWidth + 10); // Width of each image + margin
-    container.style.width = `${totalImageWidth}px`;
-
-    // Start the animation
-    moveImages();
-  }
-  
-  let currentX = 0;
-
-  function moveImages() {
-    const container = document.querySelector(".image-container");
-    const containerWidth = container.clientWidth;
-
-    // Calculate the duration based on the total width of images and the container width
-    const imageCount = imageUrls.length;
-    const totalImageWidth = imageCount * (containerWidth / 4); // Display 4 images at a time
-    const duration = (totalImageWidth - containerWidth) * 11; // Adjust '10' to control the animation speed
-
-    // Calculate the distance to move based on time
-    currentX = (currentX - 1) % totalImageWidth;
-    if (currentX === -(totalImageWidth - containerWidth)) {
-      currentX = 0;
-    }
-
-    // Move the container
-    container.style.transform = `translateX(${currentX}px)`;
-
-    // Request the next frame
-    requestAnimationFrame(moveImages);
-  }
-
-  function openModal(imageUrl) {
-    const modal = document.getElementById("modal");
-    const modalImage = document.getElementById("modalImage");
-    const closeBtn = document.getElementById("close");
-
-    modalImage.style.backgroundImage = `url(${imageUrl})`;
-    modal.style.display = "block";
-
-    closeBtn.onclick = () => closeModal();
-    modal.onclick = (event) => {
-      if (event.target === modal) {
-        closeModal();
-      }
-    };
-  }
+  };
+}
 
   function closeModal() {
     const modal = document.getElementById("modal");
@@ -181,7 +228,7 @@ function plusSlides(n) {
 }
 
 function currentSlide(n) {
-  slideIndex = n;
+    slideIndex = n;
   showSlides();
 }
 
@@ -195,3 +242,9 @@ dotsArray.forEach((dot, index) => {
 
 showSlides(); // To display the first slide
 setInterval(() => plusSlides(1), 3000); // Automatic sliding
+
+//show schedule when clicked
+function showtable(){
+  table = document.querySelector('.schedule')
+  table.classList.add('clicked')
+}
